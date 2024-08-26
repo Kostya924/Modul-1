f8e6c51b5fb99753f9530a2c8cff0725b64765e3
Kostya924 <podsekaev68@mail.ru>
2024-08-26 11:31:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+print  ("Hello World")
+# "1st program"
+print (9 ** 0.5 * 5)
+# "2nd program"
+print (9.99 > 9.98, 1000 != 1000.1)
+# "3rd program"
+print (2 * 2 + 2)
+print (2 * (2 + 2))
+print (2 * 2 + 2 == 2 * (2 + 2))
+# "4th program"
+print (float("123.456"))
+print (123.456 * 10)
+name = "123.456"
+print(name[4])
+# индексация строк
+example = "Топинамбур"
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1],example[3],example[5],example[7],example[9])
+# Переменные
+name_of_course = "Курс:Python"
+total_tasks = "Всего задач:12"
+total_hours = "Количество затраченных часов:1.5"
+time_for_task = "Среднее время выполнения: (0.125)"
+print(name_of_course,total_tasks,total_hours,time_for_task)
+# Динамическая типизация
+name = "Kostya"
+print (name, type (name))
+age = 55
+new_age = 45
+print(age + new_age)
+is_student = True
+print (is_student, type (is_student))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
